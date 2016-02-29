80cd52bffcc9be2d593c3db43b719e3edf97736a
brnaldomesi <brnaldomesi1@gmail.com>
2016-02-29 18:01:53 +0300

    (feature) Create login page

---
+++ public/static/js/app.js
@@ -0,0 +1,19 @@
+var vistagrid = angular.module('vistagrid', ['ngRoute', 'ngResource']);
+
+vistagrid.config(function ($interpolateProvider) {
+	$interpolateProvider.startSymbol('[[');
+	$interpolateProvider.endSymbol(']]');
+})
+vistagrid.config(['$resourceProvider', function ($resourceProvider) {
+	$resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+vistagrid.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'static/views/login.html'
+	});
+	// .otherwise({
+	// 	redirectTo: '/'
+	// });
+});
