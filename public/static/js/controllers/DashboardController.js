6c4311458816e9b9ca0cd356ae93a5292507ea53
brnaldomesi <brnaldomesi1@gmail.com>
2016-03-08 10:19:27 +0300

    (feature) Create view responsiveness logic
    - Abstract code to generate thumbnails to a function to allow for code
    reuse when uploaded photos are clicked.
    - Abstract code to fetch uploaded photos to a function to allow for
    code reuse when an image is uploaded
    - Create logic to upload new photos from the view

--- public/static/js/controllers/DashboardController.js
+++ public/static/js/controllers/DashboardController.js
@@ -1,16 +1,31 @@
 vistagrid.controller('DashboardController',
-	['$scope', 'PhotoService', function ($scope, PhotoService) {
-		PhotoService.Uploads.getAll().$promise.then(
-			function (response) {
-				$scope.uploads = response;
-				console.log($scope.uploads);
-			},
-			function (error) {
-				console.log(error);
-			}
-		);
+	['$scope', 'PhotoService', '$timeout', 'Upload', function ($scope, PhotoService, $timeout, Upload) {
+		$scope.uploader = {};
+		var refreshThumbnails = function () {
+			PhotoService.Thumbnails.getAll().$promise.then(
+				function (response) {
+					$scope.thumbnails = response;
+				},
+				function (error) {
 
-		$scope.uploadClicked = function (photo_id) {
+				}
+			);
+		}
+
+		var fetchUploads = function () {
+			PhotoService.Uploads.getAll().$promise.then(
+				function (response) {
+					$scope.uploads = response;
+				},
+				function (error) {
+					console.log(error);
+				}
+			);
+		}
+
+		fetchUploads();
+
+		$scope.uploadClicked = function (photo_id, path) {
 			var data = {
 				photo_id: photo_id
 			};
@@ -23,5 +38,40 @@ vistagrid.controller('DashboardController',
 
 				}
 			);
+			data = {
+				path: path
+			};
+			console.log(data);
+			PhotoService.Thumbnails.create(data).$promise.then(
+				function (response) {
+					console.log(response);
+					refreshThumbnails();
+				},
+				function (error) {
+					console.log(error);
+				}
+			);
 		};
+
+		$scope.uploadNewPhoto = function (file, errFiles) {
+			if (file) {
+				console.log(file);
+				var data = {
+					url: '/api/photos/',
+					data: {
+						path: file,
+						filter_effects: 'BLUR'
+					}
+				}
+				Upload.upload(data).then(
+					function (response) {
+						console.log('Successful upload!!');
+						fetchUploads();
+					},
+					function (error) {
+						console.log(error);
+					}
+				);
+			}
+		}
 }]);
