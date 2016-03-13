656267d84389d5f59186ae02f635cb1384e806a4
brnaldomesi <brnaldomesi1@gmail.com>
2016-03-13 20:04:05 +0300

    (feature) Create initial tests

---
+++ tests/test_base.py
@@ -0,0 +1,46 @@
+import os
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.core.files.images import ImageFile
+from PIL import Image
+from faker import Factory
+
+
+class TestBaseClass(TestCase):
+	"""Base class for all the tests in this app."""
+
+	def setUp(self):
+		"""Create objects which will be used throughout the tests."""
+		self.client = Client()
+		self.fake = Factory.create()
+		self.username = self.fake.user_name()
+		self.password = self.fake.password()
+		self.user = User.objects.create_user(
+			username=self.username,
+			password=self.password
+		)
+		self.test_image = Image.new(mode='L', size=(400, 576), color=0)
+		self.test_image.save('test.jpg')
+
+	def tearDown(self):
+		"""Destroy resources after they have been used in the tests."""
+		del self.fake
+		self.user.delete()
+		del self.test_image
+		if os.path.exists('test.jpg'):
+			os.remove('test.jpg')
+
+	def login_user(self):
+		"""Authenticate 'self.user'."""
+		url = '/api-auth/login/'
+		data = {
+			'username': self.username,
+			'password': self.password
+		}
+		self.client.post(url, data=data)
+
+	def uploadable_image(self):
+		"""Create an uploadable object from the 'test.jpg' file on disk."""
+		f = open('test.jpg')
+		imf = ImageFile(f)
+		return imf
---
+++ tests/test_photo_route.py
@@ -0,0 +1,129 @@
+from random import random
+from test_base import TestBaseClass
+from app.models import FILTERS, Photo
+
+
+class TestPhotoAPIRoute(TestBaseClass):
+	"""Test the '/api/photos/' url."""
+
+	def get_random_filter(self):
+		"""Return a random filter effect to use on photo."""
+		filters = FILTERS.keys()
+		rand_index = int(random() * len(filters))
+		return filters[rand_index]
+
+	def test_successful_upload(self):
+		"""Test successful POST request to '/api/photos/' url."""
+		url = '/api/photos/'
+		# authenticate the user
+		self.login_user()
+		# attempt upload
+		data = {
+			'path': self.uploadable_image(),
+			'filter_effects': 'BLUR'
+		}
+		response = self.client.post(url, data=data)
+		self.assertTrue(response.status_code, 201)
+		self.assertTrue(response.status_text, 'Created')
+		self.assertTrue(
+			'Success' in response.data.get('status')
+		)
+
+	def test_unauthenticated_upload(self):
+		"""Test unauthenticated POST request to '/api/photos/' url."""
+		url = '/api/photos/'
+		# attempt upload
+		data = {
+			'path': self.uploadable_image(),
+			'filter_effects': 'BLUR'
+		}
+		response = self.client.post(url, data=data)
+		self.assertTrue(response.status_code, 401)
+		self.assertTrue(response.status_text, 'Unauthorized')
+		self.assertTrue(
+			'credentials were not provided' in response.data.get('detail')
+		)
+
+	def test_upload_no_photo_selected(self):
+		"""Test authenticated POST request to '/api/photos/' url when no upload
+		photo has been specified.
+		"""
+		url = '/api/photos/'
+		# authenticate the user
+		self.login_user()
+		# attempt upload
+		data = {
+			'filter_effects': 'BLUR'
+		}
+		response = self.client.post(url, data=data)
+		self.assertTrue(response.status_code, 400)
+		self.assertTrue(response.status_text, 'Bad Request')
+		self.assertTrue(
+			'No file was submitted' in response.data.get('path')[0]
+		)
+
+	def test_upload_no_effect_specified(self):
+		"""Test authenticated POST request to '/api/photos/' url when no effect
+		has been specified.
+		"""
+		url = '/api/photos/'
+		# authenticate the user
+		self.login_user()
+		# attempt upload
+		data = {
+			'path': self.uploadable_image()
+		}
+		response = self.client.post(url, data=data)
+		self.assertTrue(response.status_code, 400)
+		self.assertTrue(response.status_text, 'Bad Request')
+		self.assertTrue(
+			'This field is required' in response.data.get('filter_effects')[0]
+		)
+
+	def test_delete_upload(self):
+		"""Test successful DELETE to '/api/photos/'."""
+		url = '/api/photos/'
+		self.login_user()
+		data = {
+			'path': self.uploadable_image(),
+			'filter_effects': 'BLUR'
+		}
+		response = self.client.post(url, data=data)
+		self.assertTrue(response.status_code, 201)
+		response = self.client.get(url)
+		photo_id = response.data[0].get('photo_id')
+		url += str(photo_id) + '/'
+		response = self.client.delete(url)
+		self.assertTrue(response.status_code, 204)
+		self.assertTrue(response.status_text, 'No Content')
+		all_photos = Photo.objects.all()
+		self.assertEqual(len(all_photos), 0)
+
+	def test_image_effect(self):
+		"""Test successful PUT to '/api/photos/' url."""
+		pass
+		# url = '/api/photos/'
+		# self.login_user()
+		# # create a photo
+		# data = {
+		# 	'path': self.uploadable_image(),
+		# 	'filter_effects': 'BLUR'
+		# }
+		# self.client.post(url, data=data)
+		# # fetch photo id
+		# response = self.client.get(url)
+		# photo_id = response.data[0].get('photo_id')
+		# url += str(photo_id) + '/'
+
+		# selected_effect = {
+		# 	'filter_effects': self.get_random_filter()
+		# }
+		# response = self.client.put(url, data=selected_effect)
+		# import ipdb; ipdb.set_trace()
+		# login
+		# post a photo
+		# fetch its id
+		# create url
+		# send a PUT
+		# assert results
+
