26de10681f0300c2555e6a14fdc8052ae8285a00
brnaldomesi <brnaldomesi1@gmail.com>
2016-03-14 13:01:35 +0300

    (feature) Configure app to deploy on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vistagrid.wsgi --log-file -
--- vistagrid/settings.py
+++ vistagrid/settings.py
@@ -152,5 +152,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'public/static'),
 )
 
+STATIC_ROOT = 'staticfiles/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
