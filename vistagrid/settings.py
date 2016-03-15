978db3ed5387479e7f312fa7580629ea569eed6d
brnaldomesi <brnaldomesi1@gmail.com>
2016-03-15 09:05:19 +0300

    (feature) Use PostgreSQL on Heroku

--- vistagrid/settings.py
+++ vistagrid/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -103,12 +104,15 @@ WSGI_APPLICATION = 'vistagrid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+if os.environ.get('DATABASE_URL'):
+	DATABASES['default'] = dj_database_url.config()
+else:
+	DATABASES['default'] = {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 
 
 # Password validation
